02a65d2627be777e763bde869023dd2e6a05508f
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2025-12-11 11:37:23 +0100

    feat: allow ssl mode in postgres

---
+++ projects/urlshortener/postgres/00-ssl-config.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+CERT_DIR="/var/lib/postgresql/certs"
+SSL_CONF="/var/lib/postgresql/ssl.conf"
+
+if [ "${POSTGRES_SSL_ENABLED:-false}" = "true" ]; then
+    echo "SSL enabled, configuring PostgreSQL to use certificates..."
+    cat > "$SSL_CONF" <<EOF
+ssl = on
+ssl_cert_file = '$CERT_DIR/server.crt'
+ssl_key_file = '$CERT_DIR/server.key'
+EOF
+    echo "SSL configuration written to $SSL_CONF"
+else
+    echo "SSL disabled"
+    cat > "$SSL_CONF" <<EOF
+ssl = off
+EOF
+fi
+
+chown postgres:postgres "$SSL_CONF"
